c0d227029b41b213e7b23437c38684ad85deb2e5
Yared Mekonnen <yaredmekonnen999@gmail.com>
2026-01-01 17:58:02 -0600

    today I learned that when long is converted to int, if it's too long it just becomes the int max

---
+++ ch01/castDoubleToInt.java
@@ -0,0 +1,8 @@
+public class castDoubleToInt {
+  public static void main(String[] args) {
+    int n = (int) Double.MAX_VALUE;
+    System.out.println(Double.MAX_VALUE);
+    System.out.println(Integer.MAX_VALUE);
+    System.out.println(n);
+  }
+}
